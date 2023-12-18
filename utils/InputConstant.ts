9ce5c902a178037acb5c438102ce4a0c2e93176f
nicolekim <nicolekim0115@gmail.com>
2023-12-18 14:25:39 +0900

    💫feat: input 컴포넌트 구현

---
+++ utils/InputConstant.ts
@@ -0,0 +1,18 @@
+export const PLACEHOLDER = {
+  email: "이메일을 입력해 주세요.",
+  password: "비밀번호를 입력해 주세요.",
+  nickname: "닉네임을 입력해 주세요.",
+  signUpPassword: "8자 이상 입력해 주세요.",
+  confirmPassword: "비밀번호를 한 번 더 입력해 주세요.",
+};
+
+export const ERROR_MESSAGE = {
+  emailRequired: "이메일을 입력해 주세요.",
+  emailInvalid: "올바른 이메일 주소가 아닙니다.",
+  emailCheck: "이메일을 확인해 주세요.",
+  passwordRequired: "비밀번호를 입력해 주세요.",
+  passwordInvalid: "8자 이상 작성해 주세요.",
+  passwordCheck: "비밀번호를 확인해 주세요.",
+  nicknameRequired: "닉네임을 입력해 주세요.",
+  nicknameInvalid: "10자 이하로 작성해 주세요.",
+};
