a6283613c99adb43ffbda7c8a79833f308d14dff
HyeyoonS <foryou9097@gmail.com>
2023-12-19 17:25:54 +0900

    💫 feat: KebabModal 구현

---
+++ components/Modal/KebabModal.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { DeviceSize } from "@/styles/DeviceSize";
+
+function KebabModal() {
+  return (
+    <KebabWrapper>
+      <KebabListWrapper>
+        <KebabList>수정하기</KebabList>
+        <KebabList>삭제하기</KebabList>
+      </KebabListWrapper>
+    </KebabWrapper>
+  );
+}
+
+export default KebabModal;
+
+const KebabWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 9.3rem;
+  height: 8.2rem;
+  border-radius: 6px;
+  border: 1px solid var(--gray-gray_D9D9D9, #d9d9d9);
+  background: var(--white-white_FFFFFF, #fff);
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.08);
+
+  @media screen and (max-width: ${DeviceSize.mobile}) {
+    width: 86px;
+    height: 74px;
+    font-size: 1.2rem;
+  }
+;
+`
+
+const KebabListWrapper = styled.ul`
+      display: column;
+    justify-content: center;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+`
+
+const KebabList = styled.li`
+      padding: 8px;
+    justify-content: center;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 1.4rem;
+    &:hover {
+      background: var(--Violet8);
+      color: var(--Violet);
+    }
+`
