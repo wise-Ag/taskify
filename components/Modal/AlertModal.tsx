1376123fe603f56a32912fdcbbf72b534963232d
HyeyoonS <foryou9097@gmail.com>
2023-12-19 20:00:38 +0900

    🛠️fix: Modal구조 수정

--- components/Modal/AlertModal.tsx
+++ components/Modal/AlertModal.tsx
@@ -8,16 +8,18 @@ interface AlertProps {
 function AlertModal({ type }: AlertProps) {
   return (
     <AlertModalWrapper>
-      {type === "incorrect" && "비밀번호가 일치하지 않습니다."}
-      {type === "complete" && "가입이 완료되었습니다!"}
-      {type === "duplicate" && "이미 사용 중인 이메일입니다."}
+      <AlertModalContents>
+        {type === "incorrect" && "비밀번호가 일치하지 않습니다."}
+        {type === "complete" && "가입이 완료되었습니다!"}
+        {type === "duplicate" && "이미 사용 중인 이메일입니다."}
+      </AlertModalContents>
     </AlertModalWrapper>
   );
 }
 
 export default AlertModal;
 
-const AlertModalWrapper = styled.div`
+const AlertModalContents = styled.div`
   // position: relative;
   width: 50.8rem;
   height: 22.2rem;
@@ -27,4 +29,10 @@ const AlertModalWrapper = styled.div`
   color: var(--Black20);
   font-size: 1.8rem;
   font-weight: 500;
+`;
+
+const AlertModalWrapper = styled.div`
+  padding: 3.2rem 2.8rem;
+  border-radius: 0.8rem;
+  background: var(--White);
 `;
--- components/Modal/ModalContainer.tsx
+++ components/Modal/ModalContainer.tsx
@@ -1,23 +1,23 @@
-import styled from "styled-components";
-import AlertModal from "./AlertModal";
+// import styled from "styled-components";
+// import AlertModal from "./AlertModal";
 
-interface ModalProps {
-  modalType: "alert" | "newDashBoard" | "todo" | "newColumn" | "manageColumn";
-}
+// interface ModalProps {
+//   modalType: "alert" | "newDashBoard" | "todo" | "newColumn" | "manageColumn";
+// }
 
-function ModalContainer({ modalType }: ModalProps) {
-  return (
-    <Wrapper>
-      <AlertModal type="incorrect" />
-      {/* <Button /> */}
-    </Wrapper>
-  );
-}
+// function ModalContainer({ modalType }: ModalProps) {
+//   return (
+//     <Wrapper>
+//       <AlertModal type="incorrect" />
+//       {/* <Button /> */}
+//     </Wrapper>
+//   );
+// }
 
-export default ModalContainer;
+// export default ModalContainer;
 
-const Wrapper = styled.div`
-  padding: 3.2rem 2.8rem;
-  border-radius: 0.8rem;
-  background: var(--White);
-`;
+// const Wrapper = styled.div`
+//   padding: 3.2rem 2.8rem;
+//   border-radius: 0.8rem;
+//   background: var(--White);
+// `;
