c36cf3c6443c24e50e0f3768775117a8c441728f
HyeyoonS <foryou9097@gmail.com>
2023-12-19 19:40:17 +0900

    💫 feat: ModalContainer 만들기
    Co-authored-by: Naeun Kim <Nico1eKim@users.noreply.github.com>

---
+++ components/Modal/ModalContainer.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import AlertModal from "./AlertModal";
+
+interface ModalProps {
+  modalType: "alert" | "newDashBoard" | "todo" | "newColumn" | "manageColumn";
+}
+
+function ModalContainer({ modalType }: ModalProps) {
+  return (
+    <Wrapper>
+      <AlertModal type="incorrect" />
+      {/* <Button /> */}
+    </Wrapper>
+  );
+}
+
+export default ModalContainer;
+
+const Wrapper = styled.div`
+  padding: 3.2rem 2.8rem;
+  border-radius: 0.8rem;
+  background: var(--white-white_FFFFFF, #fff);
+`;
