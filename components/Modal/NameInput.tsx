bd58e25dd46bcd19dfd048cec2d9b0013e00dbed
HyeyoonS <foryou9097@gmail.com>
2023-12-20 17:25:28 +0900

    🎨  UI: NewDashBoardModal 미디어쿼리 적용

--- components/Modal/NameInput.tsx
+++ components/Modal/NameInput.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DeviceSize } from "@/styles/DeviceSize";
 
 interface NameModalProps {
   titleType: "이름" | "대시보드 이름";
@@ -20,18 +21,37 @@ const NameType = styled.div`
   margin-bottom: 1rem;
   font-size: 1.8rem;
   font-weight: 500;
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    margin-top: 2.4rem;
+  }
 `;
 
 const NameInputBox = styled.input`
   width: 48.4rem;
   height: 4.8rem;
+
+  outline-color: var(--Grayd9);
+  border: 1px solid var(--Grayd9);
   padding: 1.5rem 1.6rem 1.4rem 1.6rem;
   border-radius: 6px;
+
   font-size: 1.6rem;
+  font-weight: 400;
 
   ::placeholder {
     content: "이름을 입력하세요";
-    color: var(--Gray30);
+
+    & :hover {
+      outline-color: var(--Grayd9);
+    }
+  }
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    width: 28.7rem;
+    height: 4.2rem;
+
+    border-radius: 8px;
   }
 `;
 const NameInputWrapper = styled.div``;
--- components/Modal/NewDashBoardModal.tsx
+++ components/Modal/NewDashBoardModal.tsx
@@ -1,19 +1,20 @@
 import styled from "styled-components";
 import ColorSelector from "@/components/Chip/DashBoardColor";
 import NameInput from "./NameInput";
+import ButtonSet from "../ButtonSet/ButtonSet";
+import { DeviceSize } from "@/styles/DeviceSize";
 
 function NewDashBoardModal() {
   return (
     <NewDashBoardModalWrapper>
       <NewDashBoardTitle>새로운 대시보드</NewDashBoardTitle>
-      {/* <DashBoardName>대시보드 이름</DashBoardName> */}
-      {/* <DashBoardInputWrapper> */}
       <NameInput titleType="이름" />
-      {/* </DashBoardInputWrapper> */}
       <ColorSelectorWrapper>
         <ColorSelector />
       </ColorSelectorWrapper>
-      <ButtonWrapper>버튼버튼</ButtonWrapper>
+      <ButtonWrapper>
+        <ButtonSet type="modalSet">{"생성"}</ButtonSet>
+      </ButtonWrapper>
     </NewDashBoardModalWrapper>
   );
 }
@@ -27,23 +28,30 @@ const NewDashBoardModalWrapper = styled.div`
   position: relative;
   border-radius: 8px;
   background: var(--White);
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    padding: 2.8rem 2rem 2.8rem 2rem;
+    width: 32.7rem;
+    height: 29.3rem;
+    border-radius: 8px;
+  }
 `;
 
 const NewDashBoardTitle = styled.div`
   font-size: 2.4rem;
   font-weight: 700;
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    font-size: 2rem;
+  }
 `;
-
-// const DashBoardName = styled.div``;
-
-// const DashBoardInputWrapper = styled.div`
-//   width: 48.4rem;
-//   height: 4.8rem;
-//   border: 1px solid var(--Gray30);
-// `;
 
 const ColorSelectorWrapper = styled.div`
   margin-top: 2.8rem;
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    margin-top: 2.4rem;
+  }
 `;
 
 const ButtonWrapper = styled.div`
@@ -51,4 +59,9 @@ const ButtonWrapper = styled.div`
   right: 2.8rem;
   bottom: 2.8rem;
   border: 1px solid var(--Gray30);
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    right: 2rem;
+    bottom: 2.8rem;
+  }
 `;
