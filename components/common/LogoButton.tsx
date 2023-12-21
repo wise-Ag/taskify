11aa3edb852c6ec1388125a35753dc9224f914a5
손지은 <wiseag@naver.com>
2023-12-21 20:04:56 +0900

    :hammer:  refactor : 해당 경고가 발생하지 않아 주석 지웠습니다
     refactor : 해당 경고가 발생하지 않아 주석 지웠습니다

--- components/common/LogoButton.tsx
+++ components/common/LogoButton.tsx
@@ -34,5 +34,3 @@ const StyledSmallLogo = styled(SmallLogo)`
     height: 2.7rem;
   }
 `;
-
-// 이 코드에서 The resource http://localhost:3000/_next/static/media/large-logo.a25645d8.svg was preloaded using link preload but not used within a few seconds from the window's load event. Please make sure it has an appropriate `as` value and it is preloaded intentionally. 이런 경고메시지가 뜨는데 해결 방법을 모르겠습니다ㅠㅠ
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import LogoButton from "@/components/common/LogoButton";
+
+function Test() {
+  return (
+    <>
+      <LogoButton />
+    </>
+  );
+}
+
+export default Test;
