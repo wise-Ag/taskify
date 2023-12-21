2b8073eb245b3e317032890f0568c2ec8232a82a
손지은 <wiseag@naver.com>
2023-12-21 22:44:30 +0900

    :hammer:  refactor(index) : test하느라 건드렸던 index.tsx 되돌림
     refactor(index) : test하느라 건드렸던 index.tsx 되돌림

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import DashboardNav from "@/components/Nav/DashboradNav";
-import MyDashboardNav from "@/components/Nav/MyDashboardNav";
-import SettingNav from "@/components/Nav/SettingNav";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 
@@ -15,9 +12,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <DashboardNav />
-      <MyDashboardNav />
-      <SettingNav />
     </>
   );
 }
