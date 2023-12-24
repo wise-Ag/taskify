fcca3baa3867811535d385f451f877868c82b15e
nicolekim <nicolekim0115@gmail.com>
2023-12-24 21:04:01 +0900

    🍀 refactor: useMemo 없애기

--- components/SignInput/PasswordInput.tsx
+++ components/SignInput/PasswordInput.tsx
@@ -1,21 +1,21 @@
 import VisibilityOff from "@/assets/icons/visibility-off.svg";
 import VisibilityOn from "@/assets/icons/visibility-on.svg";
 import Input, { InputProps } from "@/components/SignInput/Input";
-import { forwardRef, useMemo, useState } from "react";
+import { forwardRef, useState } from "react";
 import styled from "styled-components";
 
 const PasswordInput = forwardRef<HTMLInputElement, InputProps>(({ label, value, hasError, errorText, onChange, onBlur, placeholder }, ref) => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const inputType = useMemo(() => (isPasswordVisible ? "text" : "password"), [isPasswordVisible]);
-  const ToggleIcon = useMemo(
-    () => <ToggleButton onClick={() => setIsPasswordVisible(!isPasswordVisible)}>{isPasswordVisible ? <VisibilityOff /> : <VisibilityOn />}</ToggleButton>,
-    [isPasswordVisible],
-  );
+  const inputType = isPasswordVisible ? "text" : "password";
+
+  const toggleButton = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
 
   return (
     <InputContainer>
       <Input ref={ref} label={label} type={inputType} value={value} hasError={hasError} errorText={errorText} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
-      {ToggleIcon}
+      <ToggleButton onClick={toggleButton}>{isPasswordVisible ? <VisibilityOff /> : <VisibilityOn />}</ToggleButton>
     </InputContainer>
   );
 });
@@ -34,4 +34,6 @@ const ToggleButton = styled.div`
   position: absolute;
   top: 4.2rem;
   right: 1.6rem;
+
+  cursor: pointer;
 `;
