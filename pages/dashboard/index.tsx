be70dc920123409b9c0e82e2fa4ba5d215a0595d
Hyeyoon <foryou9097@gmail.com>
2023-12-27 08:51:13 +0900

    🧹 chore: 하이드레이션 오류

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,11 +1,23 @@
+import { styled } from "styled-components";
 import Columns from "@/components/Dashboard/Column/Columns";
+import DashboardNav from "@/components/common/Nav/DashboradNav";
+import SideMenu from "@/components/common/SideMenu/SideMenu";
 
 const DashBoardPage = () => {
   return (
     <div>
+      <DashboardNav />
       <Columns />
+      <SideMenuWrapper>
+        <SideMenu />
+      </SideMenuWrapper>
     </div>
   );
 };
 
 export default DashBoardPage;
+
+const SideMenuWrapper = styled.div`
+  position: fixed;
+  top: 0;
+`;
