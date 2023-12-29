0a53530d893d268e1adf86dfe6814322f31a5265
손지은 <wiseag@naver.com>
2023-12-29 10:39:12 +0900

    🍀 refactor: api 수정

--- api/users/index.ts
+++ api/users/index.ts
@@ -5,7 +5,7 @@ import { UserData, PostImageData, PostProfileImageProps, PostUsersProps } from "
 export const getUsers = async ({
   token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsInRlYW1JZCI6IjEtMDgiLCJpYXQiOjE3MDM1NzU1MjgsImlzcyI6InNwLXRhc2tpZnkifQ.vPTurAcm35kevcT9alVW2SxsjFcaKqnmd_mpgVwWfRU",
 }: {
-  token: string;
+  token: string | null;
 }): Promise<UserData | null> => {
   try {
     const res = await instance.get(ENDPOINTS.USERS.GET, {
