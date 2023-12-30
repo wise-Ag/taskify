c8238505a9dd1caab9767681e96fa10b96ff147b
손지은 <wiseag@naver.com>
2023-12-30 12:42:30 +0900

    🍀 refactor: color상수 constant파일로 분리

--- components/common/Chip/Tag.tsx
+++ components/common/Chip/Tag.tsx
@@ -1,18 +1,6 @@
+import { TAG_COLOR } from "@/constants/ColorConstant";
 import { DeviceSize } from "@/styles/DeviceSize";
 import styled from "styled-components";
-
-const TAG_COLOR = [
-  { bgColor: "#ffd3aa", textColor: "#d35405" },
-  { bgColor: "#dcb4eb", textColor: "#9600fa" },
-  { bgColor: "#e1fca1", textColor: "#129127" },
-  { bgColor: "#f7dbf0", textColor: "#d549b6" },
-  { bgColor: "#ffb7c5", textColor: "#c6172c" },
-  { bgColor: "#99d39f", textColor: "#00791a" },
-  { bgColor: "#a2b5ff", textColor: "#060078" },
-  { bgColor: " #d7bb9e", textColor: "#332811" },
-  { bgColor: "#fff8ae", textColor: "#ceb900" },
-  { bgColor: "#cdf3ff", textColor: "#0c41e1" },
-];
 
 const Tag = ({ tag }: { tag: string }) => {
   const tagNum = tag?.charCodeAt(0) % 10;
